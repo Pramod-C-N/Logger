073d9bc8b0cdebea1739a644d59766d52bc1b1b5
Pramod-C-N <142775603+Pramod-C-N@users.noreply.github.com>
2023-09-27 16:04:51 +0530

    Nlog Add

---
+++ LoggerService/ILoggersManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerService
+{
+    public interface ILoggersManager
+    {
+        void LogError(Exception ex, string message);
+        void LogWarning(string message);
+        void LogDebug(string message);
+        void LogInfo(string message);
+    }
+}
---
+++ LoggerService/LoggersManager.cs
@@ -0,0 +1,34 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerService
+{
+    public class LoggersManager : ILoggersManager
+    {
+       private static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        public void LogDebug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void LogError(Exception ex, string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            _logger.Warn(message);
+        }
+    }
+}
